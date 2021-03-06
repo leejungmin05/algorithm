dbe41451bcf3aacbdfbcaab5cd47f97a67e1e34c
JungMin LEE <leejungmin05@gmail.com>
2021-03-07 02:09:01 +0900

    refactor stack

--- src/stack/Stack.kt
+++ src/stack/Stack.kt
@@ -2,6 +2,7 @@ package interface_form
 
 import java.lang.Exception
 
+
 class Stack<E> : StackInterface<E> {
     var array: Array<E> = Array(10) { i -> Any() } as Array<E>
 
@@ -9,39 +10,50 @@ class Stack<E> : StackInterface<E> {
 
 
     override fun push(item: E): E {
-        //top<size
-
-        array[++top] = item
-        //top==size
+        if(!isFull()) {
+            array[++top] = item
+        } else {
+            val newArray = Array(array.size*2){i->Any()} as Array<E>
+            array.copyInto(newArray)
+            newArray[++top] = item
+            array = newArray
+        }
         return item
+    }
+
+    fun isFull():Boolean{
+        return top >= array.size -1
     }
 
     override fun pop(): E {
         if(empty()){
-            throw Exception()
+            throw Exception("stack is Empty ")
         }
-        //top==0
-        //throw Exception()
-
         return array[top--]
     }
 
     override fun peek(): E {
+        if(empty()){
+            throw Exception("stack is Empty ")
+        }
         return array[top]
     }
 
     override fun search(value: Any?): Int {
+        if(empty()){
+            throw Exception("stack is Empty ")
+        }
         //위에서 아래로
         for (i in top downTo 0) {
             if (value == array[i]) {
-                return top - i
+                return top+1 - i
             }
         }
-        return -1
+        return -1 // value가 없는 경우
     }
 
     override fun size(): Int {
-        return top
+        return top+1
     }
 
     override fun clear() {
@@ -49,7 +61,7 @@ class Stack<E> : StackInterface<E> {
     }
 
     override fun empty(): Boolean {
-        return top == 0
+        return top == -1
     }
 
     override fun toString() : String {
@@ -57,7 +69,7 @@ class Stack<E> : StackInterface<E> {
         for(i in 0..top) {
            result += (array[i].toString()+" ")
         }
-       return result
+    return result
     }
 }
 
--- src/stack/test.kt
+++ src/stack/test.kt
@@ -1,40 +1,79 @@
 package interface_form
 
+import com.sun.jmx.snmp.SnmpUnsignedInt
+import java.lang.Exception
+import java.util.*
+
 fun main() {
     val stack = Stack<Int>()
     stack.push(3)
     stack.push(5)
     stack.push(7)
     stack.push(8)
+    stack.push(4)
+    stack.push(8)
+    stack.push(1)
+    stack.push(6)
+    stack.push(7)
 
-    println(stack.pop())
-    println(stack.pop())
+
+//    println(stack.pop())
+//    println(stack.pop())
+
 
     println(stack.toString())
+
+
     while(true){
-        println(
-            "명령어를 입력해주세요\n"+
-                    "1 : push 2: pop"
+        print(
+            "\n명령어를 입력해주세요\n"+
+                    "1 : push 2: pop 3: peek 4: clear 5:search 6: size 7: empty 8: print 9: exit \n >>>"
         )
-        val a = readLine()!!.toInt()
+        val a = try {readLine()!!.toInt()} catch (e : NumberFormatException) {println("숫자를 입력하세요")}
+
         when(a) {
             1-> {
-                val item = readLine()!!.toInt()
-                stack.push(item)
+                print("push 할 아이템을 입력하세요 : ")
+                val item = try{
+                    readLine()!!.toInt()
+                } catch(e : NumberFormatException) {
+                    println("숫자를 입력하세요")
+                    continue
+                }
+                    stack.push(item)
+                    println(stack.toString())
             }
-            2-> stack.pop()
-            3-> println(stack.toString())
-            4-> return
+            2-> {
+                println("가장 위의 아이템을 제거합니다")
+                try {
+                    stack.pop()
+                }catch (e:Exception){
+                    println("스택이 비어있습니다.")
+                }
+            }
+            3-> {
+                println("stack 가장 위의 아이템입니다")
+                println(stack.peek())
+            }
+            4-> {
+                println("스택의 모든 아이템을 삭제합니다")
+                stack.clear()
+            }
+            5-> {
+                println("찾고싶은 아이템을 입력하세요")
+                val item = try{ readLine()!!.toInt() } catch(e : NumberFormatException) {println("숫자를 입력하세요")}
+                if(stack.search(item)==-1) {
+                    println("입력하신 아이템이 없습니다")
+                }else
+                println(stack.search(item))
+            }
+            6-> println("stack의 사이즈는 " + stack.size() + "입니다")
+            7-> println(stack.empty())
+            8-> if(stack.empty()) println("stack이 비어있습니다")
+                else println(stack.toString())
+
+            9-> return
 
         }
     }
 }
-/***
- * 1 : push
- * 2 : pop
- * 3 : print
- * clear
- * serach
- * peek
- * 4 : exit
-        ***/
