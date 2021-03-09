31f6df8d112ac3d13530358ecccbbdc224bcfb71
JungMin LEE <leejungmin05@gmail.com>
2021-03-09 20:27:12 +0900

    queue interface implementation

---
+++ src/queue/QueueInterface.kt
@@ -0,0 +1,14 @@
+package queue
+
+interface QueueInterface<E> {
+
+    // 큐의 가장 마지막에 요소 추가
+    fun offer(e: E): Boolean
+
+    // 큐의 첫번째 요소 삭제하고 삭제된 요소 반환
+    fun poll(): E
+
+    //큐의 첫번째 요소 반환
+    fun peek(): E
+
+}
